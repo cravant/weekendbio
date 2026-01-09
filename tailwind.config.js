debc3d53a1a7392cbbd52135065e0f676e52f304
cravant <joecool@brantborden.com>
2026-01-08 22:38:14 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brandRed: "#e1062c",
+        brandTeal: "#5eead4",
+        bgDark: "#0b0f14",
+        cardDark: "#121826"
+      }
+    }
+  },
+  plugins: []
+}
