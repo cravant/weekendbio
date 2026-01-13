04dfcec9f49f81998e470268360124fb35b481b0
cravant <joecool@brantborden.com>
2026-01-13 08:53:17 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById('modal');
+
+
+function openModal() {
+modal.classList.remove('hidden');
+}
+
+
+function closeModal() {
+modal.classList.add('hidden');
+localStorage.setItem('wb_seen', 'true');
+}
+
+
+if (localStorage.getItem('wb_seen')) {
+modal.classList.add('hidden');
+}
