bc412371835b4413fb5b1bb09a63bb87874a4be7
cravant <joecool@brantborden.com>
2026-01-13 09:23:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const modal = document.getElementById('modal');
 
-
 function openModal() {
-modal.classList.remove('hidden');
+  modal.classList.remove('hidden');
 }
-
 
 function closeModal() {
-modal.classList.add('hidden');
-localStorage.setItem('wb_seen', 'true');
+  modal.classList.add('hidden');
+  localStorage.setItem('wb_seen', 'true');
 }
 
-
 if (localStorage.getItem('wb_seen')) {
-modal.classList.add('hidden');
+  modal.classList.add('hidden');
 }
